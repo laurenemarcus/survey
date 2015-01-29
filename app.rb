60e20f9b85585c4c4dccea7bbcdb0b834fa75579
Lauren Marcus & Marc Lee <student@epicodus.com>
2015-01-28 16:50:23 -0800

    Take user to next page to rename title, delete the survey, and edit questions.

--- app.rb
+++ app.rb
@@ -19,28 +19,59 @@ end
 
 post("/survey_questions") do
   survey_question = params.fetch("survey_question")
-  @survey_question = SurveyQuestion.create({:survey_question => survey_question})
+  survey_title_id = params.fetch("survey_title_id")
+  @survey_question = SurveyQuestion.create({:survey_question => survey_question, :survey_title_id => survey_title_id})
   @survey_questions = SurveyQuestion.all()
   @survey_titles = SurveyTitle.all()
   erb(:index)
 end
 
-get("/survey_questions/:id/edit") do
-  @survey_question = Question.find(params.fetch("id").to_i())
-  erb(:survey_question)
+get("/titles/:id/edit") do
+  @survey_title = SurveyTitle.find(params.fetch("id").to_i())
+  @survey_questions = @survey_title.survey_questions()
+  @survey_titles = SurveyTitle.all()
+  erb(:survey_edit)
 end
 
-patch("/survey_question/:id") do
+patch("/survey_title/:id") do
+  survey_title = params.fetch("survey_title")
+  @survey_title = SurveyTitle.find(params.fetch("id").to_i())
+  @survey_title.update({:survey_title => survey_title})
+  @survey_titles = SurveyTitle.all()
+  @survey_questions = SurveyQuestion.all()
+  erb(:survey_edit)
+end
+
+delete("/survey_title/:id") do
+  @survey_title = SurveyTitle.find(params.fetch("id").to_i())
+  @survey_title.delete()
+  @survey_titles = SurveyTitle.all()
+  @survey_questions = SurveyQuestion.all()
+  erb(:index)
+end
+
+get("/survey_questions/:id/edit") do
+  @survey_question = SurveyQuestion.find(params.fetch("id").to_i())
+  @survey_questions = SurveyQuestion.all()
+  @survey_titles = SurveyTitle.all()
+  erb(:survey_edit)
+end
+
+patch("/survey_questions/:id") do
   survey_question = params.fetch("survey_question")
   @survey_question = SurveyQuestion.find(params.fetch("id").to_i())
   @survey_question.update({:survey_question => survey_question})
-  @survey_question = SurveyQuestion.all()
-  erb(:survey_question)
+  @survey_questions = SurveyQuestion.all()
+  @survey_titles = SurveyTitle.all()
+  @survey_title = @survey_question.survey_title()
+
+  erb(:survey_edit)
 end
 
-delete("/survey_question/:id") do
+delete("/survey_questions/:id") do
   @survey_question = SurveyQuestion.find(params.fetch("id").to_i())
   @survey_question.delete()
   @survey_questions = SurveyQuestion.all()
-  erb(:survey_question)
+  @survey_titles = SurveyTitle.all()
+  erb(:index)
 end
